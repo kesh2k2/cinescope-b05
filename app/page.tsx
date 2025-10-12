93d8d06b9e659e55f3f36ee3a27ab9b3e3b01dab
kesh2k2 <akeshdulanjana@gmail.com>
2025-10-12 16:08:57 +0530

    Latest update with shadcn init config

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
 
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
-    style={{backgroundColor:"green"}}
+    style={{backgroundColor:"blue"}}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
