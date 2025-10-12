39491693d15d227f6b877f8904462ec25bc5ebdd
kesh2k2 <akeshdulanjana@gmail.com>
2025-10-12 16:03:02 +0530

    git

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
   return (
 
 
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
+    style={{backgroundColor:"green"}}
+    >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -16,7 +18,7 @@ export default function Home() {
           priority
         />
 
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left" style={{color:"yellow"}}>
           <li>
             {greet}
           </li>
